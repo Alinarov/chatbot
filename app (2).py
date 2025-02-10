e07963cdda7c632da96664902b482f979f31f232
Alinarov <150054498+Alinarov@users.noreply.github.com>
2025-02-10 16:55:01 -0500

    Add files via upload

---
+++ app (2).py
@@ -0,0 +1,46 @@
+import streamlit as st
+from openai import OpenAI
+
+class Page1:
+  def __init__(self) -> None:
+    from secret_keys import open_api_key
+    self.client = OpenAI(api_key=open_api_key)
+
+    prompt = "piensa como un mapache cuak cuak"
+
+    if "trip_adviser_messages" not in st.session_state:
+      st.session_state["trip_adviser_messages"] = [
+          {"rote": "system", "content": prompt}
+      ]
+
+
+    def communicate():
+
+      
+      client = OpenAI(api_key = st.secrets.OpenAIAPI.openai_api_key)
+
+      if "messages" not in st.session_state:
+        st.session_state["messages"] = [  
+          {"role":"system", "content":"piensa en comida"}
+        ]
+      messages = st.session_state["messages"]
+      user_message = {"role":"user", "content": st.session_state["user_input"]}
+      messages.append(user_message)
+
+      response = client.chat.completions.create(
+          model="gpt-3.5-turbo",
+          messages=messages
+      )
+
+      bot_message = response.choices[0].message
+      messages.append(bot_message)
+
+
+
+def main():
+  sidebar = st.sidenat
+
+  page = sidebar.radio("seleccionar chatbot", ["Trip Adviser"])
+
+  if page == "Trip Adviser":
+    Page1()

ba54dfae8c3a9765f5af5881aeacad930e8564f2
Alinarov <150054498+Alinarov@users.noreply.github.com>
2025-02-10 17:45:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ except:
 
 class Page1:
   def __init__(self) -> None:
-    from secret_keys import open_api_key
+    #from secret_keys import open_api_key
     self.client = OpenAI(api_key=open_api_key)
 
     prompt = "piensa como un mapache cuak cuak"
@@ -42,7 +42,7 @@ class Page1:
 
 class Page2:
   def __init__(self) -> None:
-    from secret_keys import open_api_key
+    #from secret_keys import open_api_key
     self.client = OpenAI(api_key=open_api_key)
 
     prompt = "piensa como un mapache cuak cuak"
